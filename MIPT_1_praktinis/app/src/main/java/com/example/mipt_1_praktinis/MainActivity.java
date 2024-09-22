043bbc19a382d2fe5f93d23469629296538ca6eb
Vilius <viliuspetronisss@gmail.com>
2024-09-22 22:17:10 +0300

    Added functionality to button in a different way

--- MIPT_1_praktinis/app/src/main/java/com/example/mipt_1_praktinis/MainActivity.java
+++ MIPT_1_praktinis/app/src/main/java/com/example/mipt_1_praktinis/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mipt_1_praktinis;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
+    Button btnChangeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnChangeText = (Button)findViewById(R.id.btnChangeText);
+        tvMain = (TextView)findViewById(R.id.tvMain);
+
+        btnChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvMain.setText("Hello");
+            }
+        });
+
     }
 }
