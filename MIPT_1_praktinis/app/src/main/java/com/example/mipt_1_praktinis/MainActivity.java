3ff699ae9cef96ae415b91dc7d31ee6471562bde
Vilius <viliuspetronisss@gmail.com>
2024-09-22 22:00:07 +0300

    Added functionality to button

--- MIPT_1_praktinis/app/src/main/java/com/example/mipt_1_praktinis/MainActivity.java
+++ MIPT_1_praktinis/app/src/main/java/com/example/mipt_1_praktinis/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mipt_1_praktinis;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void btnChangeTextClick(View view) {
+        TextView tvMain = (TextView)findViewById(R.id.tvMain);
+        tvMain.setText("Hello");
     }
 }
